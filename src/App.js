3e18abf0420e2a26014fc41ab9a0a2eacd8632c6
Nurzhigit <Nurjigit00a@gmail.com>
2024-07-23 13:10:16 +0500

    done almost

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import Card from "./components/Card/Card";
 import Drawer from "./components/Drawer";
 import Header from "./components/Header";
@@ -6,15 +7,14 @@ import Header from "./components/Header";
 function App() {
   const [items, setItems] = React.useState([]);
   const [cartItems, setCartItems] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState("");
   const [cartOpened, setCartOpened] = React.useState(false);
 
   React.useEffect(() => {
-    fetch("https://66977b9502f3150fb66dc8d4.mockapi.io/items")
+    axios
+      .get("https://66977b9502f3150fb66dc8d4.mockapi.io/items")
       .then((res) => {
-        return res.json();
-      })
-      .then((json) => {
-        setItems(json);
+        setItems(res.data);
       });
   }, []);
 
@@ -24,6 +24,10 @@ function App() {
 
   console.log(cartItems);
 
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
+  };
+
   return (
     <div className="wrapper">
       {cartOpened ? (
@@ -32,7 +36,9 @@ function App() {
       <Header onClickCart={() => setCartOpened(true)} />
       <div className="content">
         <div className="content-search">
-          <h1>Все кроссовки</h1>
+          <h1>
+            {searchValue ? `Поиск по запросу: ${searchValue}` : "Все кроссовки"}
+          </h1>
           <div className="search-block">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -45,19 +51,37 @@ function App() {
                 d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1S492.1 112 412 112s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6M570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4"
               />
             </svg>
-            <input type="text" placeholder="Поиск..." />
+            {searchValue && (
+              <img
+                onClick={() => setSearchValue("")}
+                className="removeBtn clear"
+                src="/img/btn-remove.svg"
+                alt=""
+              />
+            )}
+            <input
+              onChange={onChangeSearchInput}
+              type="text"
+              placeholder="Поиск..."
+              value={searchValue}
+            />
           </div>
         </div>
         <div className="content-sneakers">
-          {items.map((item) => (
-            <Card
-              name={item.name}
-              price={item.price}
-              imageUrl={item.imageUrl}
-              onClickFavorite={() => console.log("Добавили в закладки")}
-              onPlus={(obj) => onAddToCart(obj)}
-            />
-          ))}
+          {items
+            .filter((item) =>
+              item.name.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((item, index) => (
+              <Card
+                key={index}
+                name={item.name}
+                price={item.price}
+                imageUrl={item.imageUrl}
+                onClickFavorite={() => console.log("Добавили в закладки")}
+                onPlus={(obj) => onAddToCart(obj)}
+              />
+            ))}
         </div>
       </div>
     </div>
